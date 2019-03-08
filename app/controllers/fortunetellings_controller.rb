78dbc35ce3426105175acb1ec481fec9d27ccb2f
keigo999 <highbridgek5@gmail.com>
2019-03-08 15:13:21 +0900

    誕生日をviewに表示

--- app/controllers/fortunetellings_controller.rb
+++ app/controllers/fortunetellings_controller.rb
@@ -1,4 +1,10 @@
 class FortunetellingsController < ApplicationController
+  include FortunetellingsHelper
+  
   def show
+    @birthday = params[:birthday]
+    @year = @birthday[0..3]
+    @month = @birthday[4..5]
+    @day = @birthday[6..8]
   end
 end
