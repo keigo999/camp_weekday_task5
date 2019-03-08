4edb28cad10d530663e69256f3e94131fc1e1891
keigo999 <highbridgek5@gmail.com>
2019-03-08 15:52:22 +0900

    誕生日判定メソッド作成

--- app/controllers/fortunetellings_controller.rb
+++ app/controllers/fortunetellings_controller.rb
@@ -1,10 +1,13 @@
 class FortunetellingsController < ApplicationController
   include FortunetellingsHelper
-  
+
   def show
     @birthday = params[:birthday]
     @year = @birthday[0..3]
     @month = @birthday[4..5]
     @day = @birthday[6..8]
+    #今日が誕生日か確認
+    your_birthday?
   end
+
 end
--- app/helpers/fortunetellings_helper.rb
+++ app/helpers/fortunetellings_helper.rb
@@ -1,2 +1,11 @@
 module FortunetellingsHelper
+  require "date"
+
+  def your_birthday?
+    today = Date.today
+    /(^\d{4})-(\d{2})-(\d{2})/ =~ today.to_s
+    if @month == $2 && @day == $3
+      @happy_birthday = true
+    end
+  end
 end
