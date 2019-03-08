381454cdcef8b84b8067f4549d4526047c3cfc69
keigo999 <highbridgek5@gmail.com>
2019-03-08 21:11:30 +0900

    レビュー箇所の修正

--- app/controllers/fortunetellings_controller.rb
+++ app/controllers/fortunetellings_controller.rb
@@ -5,10 +5,10 @@ class FortunetellingsController < ApplicationController
     @birthday = params[:birthday]
     @year = @birthday[0..3]
     @month = @birthday[4..5]
-    @day = @birthday[6..8]
+    @day = @birthday[6..7]
     #今日が誕生日か確認
     your_birthday?
-    #年齢を確認
+    #星座を確認
     constellation(@month, @day)
     #運勢をランダムに表示
     @fortune = %w(大吉 中吉 吉 小吉 凶 大凶).shuffle[1]
--- app/helpers/fortunetellings_helper.rb
+++ app/helpers/fortunetellings_helper.rb
@@ -13,33 +13,33 @@ module FortunetellingsHelper
   #my_dayにstring型で連結し、その後整数に変換(例 "02" + "22" = "222" → 222)
   def constellation(month, day)
     my_day = (month + day).to_i
-    @mine = if my_day.between?(120, 218)
-              "水瓶座"
+    @z_sign = if my_day.between?(120, 218)
+              "水瓶"
             elsif my_day.between?(219, 320)
-              "魚座"
+              "魚"
             elsif my_day.between?(321, 419)
-              "牡羊座"
+              "牡羊"
             elsif my_day.between?(420, 520)
-              "牡牛座"
+              "牡牛"
             elsif my_day.between?(521, 621)
-              "双子座"
+              "双子"
             elsif my_day.between?(622, 722)
-              "蟹座"
+              "蟹"
             elsif my_day.between?(723, 822)
-              "獅子座"
+              "獅子"
             elsif my_day.between?(823, 922)
-              "乙女座"
+              "乙女"
             elsif my_day.between?(923, 1023)
-              "てんびん座"
+              "てんびん"
             elsif my_day.between?(1024, 1122)
-              "蠍座"
+              "蠍"
             elsif my_day.between?(1123, 1221)
-              "射手座"
+              "射手"
             elsif my_day.between?(1222, 1231) || my_day.between?(101, 109)
-              "山羊座"
+              "山羊"
             end
   end
-
+  #年齢を確認
   def calcage(birthdayStr)
     if birthdayStr !~ /^[0-9]{8}$/
       return -1
