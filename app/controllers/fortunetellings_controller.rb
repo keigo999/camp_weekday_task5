2fd0de32f2924723315038a67227e9e75e0e03c2
keigo999 <highbridgek5@gmail.com>
2019-03-08 18:02:04 +0900

    運勢占いメソッド作成

--- app/controllers/fortunetellings_controller.rb
+++ app/controllers/fortunetellings_controller.rb
@@ -8,8 +8,8 @@ class FortunetellingsController < ApplicationController
     @day = @birthday[6..8]
     #今日が誕生日か確認
     your_birthday?
+    #年齢を確認
     constellation(@month, @day)
-
+    @fortune = %w(大吉 中吉 吉 小吉 凶 大凶).shuffle[1]
   end
-
 end
